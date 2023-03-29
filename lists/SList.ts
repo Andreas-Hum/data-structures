5b792e6283519d24a7d1fe3e46ece42d9a2e38b5
Andreas-Hum <heyoallos@gmail.com>
2023-03-29 16:56:17 +0200

    Unshift and push created

--- lists/SList.ts
+++ lists/SList.ts
@@ -17,19 +17,22 @@ export default class SinglyLinkedList<T> {
     */
     private _tail: Node<T> | null = null;
 
+    public length: number;
+
     /**
         Creates an instance of SinglyLinkedList.
     */
     constructor() {
         this._head = null;
         this._tail = null;
+        this.length = 0;
     }
 
     /**
         Returns the first node of the list.
         @returns {Node<T>|null} The first node of the list or null if the list is empty.
     */
-    peakFirst(): Node<T> | null {
+    peek(): Node<T> | null {
         return this._head;
     }
 
@@ -37,7 +40,57 @@ export default class SinglyLinkedList<T> {
         Returns the last node of the list.
         @returns {Node<T>|null} The last node of the list or null if the list is empty.
     */
-    peakLast(): Node<T> | null {
+    peakTail(): Node<T> | null {
         return this._tail;
     }
+
+
+    //ADDING ELEMENTS TO THE LIST
+
+    /**
+     * Appends new elements to the end of an list, and returns the new length of the list.
+     * @param items — New elements to add to the list.
+     * @returns The length of the list
+    */
+    push(...data: T[]): number {
+        let current_tail: Node<T> | null = this._tail;
+
+        for (let i = 0; i < data.length; i++) {
+            const new_node: Node<T> = new Node<T>(data[i]);
+
+            if (current_tail) {
+                current_tail.setNext(new_node);
+            }
+
+            current_tail = new_node;
+            this.length += 1;
+        }
+
+        this._tail = current_tail;
+        return this.length;
+    }
+
+    /**
+     * Appends new elements to the start of an list, and returns the new length of the list.
+     * @param items — New elements to add to the list.
+     * @returns The length of the list
+    */
+    unshift(...data: T[]): number {
+        let current_head: Node<T> | null = this._head;
+
+        for (let i = 0; i < data.length; i++) {
+            const new_node: Node<T> = new Node<T>(data[i]);
+
+            if (current_head) {
+                new_node.setNext(current_head);
+            }
+
+            current_head = new_node;
+            this.length += 1;
+        }
+
+        this._head = current_head;
+        return this.length;
+    }
 }
+
