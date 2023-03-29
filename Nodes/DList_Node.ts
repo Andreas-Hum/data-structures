f126ced4c624963c301bf3f9d119e6fe0bd3e1c5
Andreas <ahumme22@student.aau.dk>
2023-03-29 13:59:37 +0200

    Doubly linked list node created

---
+++ Nodes/DList_Node.ts
@@ -0,0 +1,87 @@
+
+
+/**
+ * Represents a node in a doubly linked list.
+ * @template T - The type of data stored in the node.
+ */
+export default class Node<T> {
+    /**
+     * The value stored in the node.
+     * @private
+     */
+    private _value: T;
+
+    /**
+     * The next node in the linked list.
+     * @private
+     */
+    private _next: Node<T> | null;
+
+    /**
+     * The previous node in the linked list.
+     * @private
+     */
+    private _prev: Node<T> | null;
+
+    /**
+     * Creates a new node with the specified data.
+     * @param data - The data to store in the node.
+     */
+    constructor(data: T) {
+        this._value = data;
+        this._next = null;
+        this._prev = null;
+    }
+
+    // Getters
+
+    /**
+     * Returns the data stored in the node.
+     * @returns The data stored in the node.
+     */
+    public getValue(): T {
+        return this._value;
+    }
+
+    /**
+     * Returns the next node in the linked list.
+     * @returns The next node in the linked list, or null if there is no next node.
+     */
+    public getNext(): Node<T> | null {
+        return this._next;
+    }
+
+    /**
+     * Returns the previous node in the linked list.
+     * @returns The previous node in the linked list, or null if there is no previous node.
+     */
+    public getPrev(): Node<T> | null {
+        return this._prev;
+    }
+
+    // Setters
+
+    /**
+     * Sets the data stored in the node.
+     * @param data - The data to store in the node.
+     */
+    public setValue(data: T): void {
+        this._value = data;
+    }
+
+    /**
+     * Sets the next node in the linked list.
+     * @param nextNode - The next node in the linked list.
+     */
+    public setNext(nextNode: Node<T>): void {
+        this._next = nextNode;
+    }
+
+    /**
+     * Sets the previous node in the linked list.
+     * @param prevNode - The previous node in the linked list.
+     */
+    public setPrev(prevNode: Node<T>): void {
+        this._next = prevNode;
+    }
+}
