aa643365d3704041504e3be1544bb17e47ff82ab
Andreas-Hum <heyoallos@gmail.com>
2023-03-29 18:03:36 +0200

    Gitignore and slinked list basics done

--- lists/SList.ts
+++ lists/SList.ts
@@ -17,22 +17,23 @@ export default class SinglyLinkedList<T> {
     */
     private _tail: Node<T> | null = null;
 
-    public length: number;
+    public length: number = 0;
 
     /**
         Creates an instance of SinglyLinkedList.
     */
-    constructor() {
-        this._head = null;
-        this._tail = null;
-        this.length = 0;
+    constructor(...items: (T | Node<T>)[]) {
+        if (items.length > 0) {
+            this.push(...items)
+        }
+
     }
 
     /**
         Returns the first node of the list.
         @returns {Node<T>|null} The first node of the list or null if the list is empty.
     */
-    peek(): Node<T> | null {
+    public peek(): Node<T> | null {
         return this._head;
     }
 
@@ -40,7 +41,7 @@ export default class SinglyLinkedList<T> {
         Returns the last node of the list.
         @returns {Node<T>|null} The last node of the list or null if the list is empty.
     */
-    peakTail(): Node<T> | null {
+    public peakTail(): Node<T> | null {
         return this._tail;
     }
 
@@ -52,21 +53,31 @@ export default class SinglyLinkedList<T> {
      * @param items — New elements to add to the list.
      * @returns The length of the list
     */
-    push(...data: T[]): number {
+    public push(...items: (T | Node<T>)[]): number {
         let current_tail: Node<T> | null = this._tail;
 
-        for (let i = 0; i < data.length; i++) {
-            const new_node: Node<T> = new Node<T>(data[i]);
+        for (let i = 0; i < items.length; i++) {
+            let new_node: Node<T>
+
+            if (items[i] instanceof Node<T>) {
+                new_node = items[i] as Node<T>;
+            } else {
+                new_node = new Node<T>(items[i] as T);
+
+            }
 
             if (current_tail) {
                 current_tail.setNext(new_node);
+            } else {
+                this._tail = new_node
+                this._head = this._tail
             }
+
 
             current_tail = new_node;
             this.length += 1;
         }
-
-        this._tail = current_tail;
+        this._tail = current_tail
         return this.length;
     }
 
@@ -75,22 +86,53 @@ export default class SinglyLinkedList<T> {
      * @param items — New elements to add to the list.
      * @returns The length of the list
     */
-    unshift(...data: T[]): number {
+    public unshift(...items: (T | Node<T>)[]): number {
         let current_head: Node<T> | null = this._head;
 
-        for (let i = 0; i < data.length; i++) {
-            const new_node: Node<T> = new Node<T>(data[i]);
+        for (let i = items.length - 1; i >= 0; i--) {
+            let new_node: Node<T>
+
+            if (items[i] instanceof Node<T>) {
+                new_node = items[i] as Node<T>;
+            } else {
+                new_node = new Node<T>(items[i] as T);
+
+            }
 
             if (current_head) {
                 new_node.setNext(current_head);
+            } else {
+                this._head = new_node
+                this._tail = this._head
             }
+
 
             current_head = new_node;
             this.length += 1;
         }
-
-        this._head = current_head;
+        this._head = current_head
         return this.length;
     }
+
+
+
+    public printElements(): void {
+        let current = this._head
+        while (current) {
+            console.log(current.getValue())
+            current = current.getNext()
+        }
+    }
 }
+
+
+const testings = new Node<number>(0)
+const test = new SinglyLinkedList<number>(4, 5, 6)
+test.push(1, 2, 3)
+test.unshift(11,9,13)
+
+// console.log(test.length)
+// console.log(test.unshift(1, 2, 3))
+test.printElements()
+
 
